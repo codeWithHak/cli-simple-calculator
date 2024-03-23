f21a1c6898359035a0705d0e96485c32a70bfd6a
Huzair Ahmed Khan <huzairahmedkhan@gmail.com>
2024-03-23 18:24:40 +0500

    initial project

---
+++ index.ts
@@ -0,0 +1,44 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+
+let answer = await inquirer.prompt([
+  { 
+    message: "Please Enter First Number",
+   type: "number",
+   name: "firstNumber" 
+ },
+
+  {
+    message: "Please Enter Second Number",
+    type: "number",
+    name: "secondNumber",
+  },
+
+  {
+    message: "Select the operator to perform operatoin",
+    type: "list",
+    name: "operator",
+    choices: ["Addition", "Subtraction", "Multiplication", "Division"],
+  },
+]);
+
+if (answer.operator === "Addition") {
+    console.log(answer.firstNumber + answer.secondNumber);
+    
+}
+
+else if (answer.operator === "Subtraction") {
+    console.log(answer.firstNumber - answer.secondNumber);
+    
+}
+
+else if (answer.operator === "Multiplication") {
+    console.log(answer.firstNumber * answer.secondNumber);
+    
+}
+
+else if (answer.operator === "Division") {
+    console.log(answer.firstNumber / answer.secondNumber);
+    
+}
