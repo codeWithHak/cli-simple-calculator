e66fa6d9d8558b3bc0668b27858c623d10af3bb9
Huzair Ahmed Khan <huzairahmedkhan@gmail.com>
2024-03-24 18:01:03 +0500

    add signs of operators

--- index.js
+++ index.js
@@ -19,14 +19,14 @@ let answer = await inquirer.prompt([
     },
 ]);
 if (answer.operator === "Addition") {
-    console.log(answer.firstNumber + answer.secondNumber);
+    console.log(answer.firstNumber + " + " + answer.secondNumber + " = " + answer.firstNumber + answer.secondNumber);
 }
 else if (answer.operator === "Subtraction") {
-    console.log(answer.firstNumber - answer.secondNumber);
+    console.log(`${answer.firstNumber} - ${answer.secondNumber} = ${answer.firstNumber - answer.secondNumber}`);
 }
 else if (answer.operator === "Multiplication") {
-    console.log(answer.firstNumber * answer.secondNumber);
+    console.log(`${answer.firstNumber} x ${answer.secondNumber} = ${answer.firstNumber * answer.secondNumber}`);
 }
 else if (answer.operator === "Division") {
-    console.log(answer.firstNumber / answer.secondNumber);
+    console.log(`${answer.firstNumber} ÷ ${answer.secondNumber} = ${answer.firstNumber / answer.secondNumber}`);
 }
